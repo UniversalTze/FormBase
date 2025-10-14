7c44f1e09ba6c7806cdfa0f70a283537bf2a67b7
Tze Kheng Goh <dgoh03625@yahoo.com>
2025-10-14 23:37:03 +1000

    Added notes about how it expo nav works.

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,10 @@
 import { Text, View } from "react-native";
 
+// when expo is used to make native apps, expo router is used to set up routes 
+// and navigations between different pages in app. 
+// app is route directory (where router expects to find all of components)
+// instead of using registerRouteComponent in expo
+// this file is rendered when application is ran (entry point)
 export default function Index() {
   return (
     <View
