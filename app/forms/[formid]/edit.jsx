ed3904495d8588f22746f44633ebd96d60d1b100
Tze Kheng Goh <dgoh03625@yahoo.com>
2025-10-24 22:04:00 +1000

    Updared Edit to be part of the [formid] path now.

--- app/forms/[formid]/edit.jsx
+++ app/forms/[formid]/edit.jsx
@@ -9,7 +9,7 @@ import { apiRequest } from "../../../api/api";
 import FormEditor from "../../../components/FormEditor";
 
 export default function EditFormScreen() {
-  const { id } = useLocalSearchParams(); // "id" from the URL (/forms/edit/123)
+  const { formid } = useLocalSearchParams(); // "id" from the URL (/forms/edit/123)
   const [form, setForm] = React.useState(null);
   const [loading, setLoading] = React.useState(true);
   const [error, setError] = React.useState(null);
@@ -22,7 +22,7 @@ export default function EditFormScreen() {
     (async () => {
       try {
         setError(null); // reset error to null for each request. 
-        const data = await apiRequest(`/form?id=eq.${id}`); // Get request
+        const data = await apiRequest(`/form?id=eq.${formid}`); // Get request
         if (mounted) setForm(data[0]);
       } catch (e) {
         if (mounted) setError(e?.message || "Failed to load form"); // set error if caught. 
@@ -31,12 +31,12 @@ export default function EditFormScreen() {
       }
     })();
     return () => { mounted = false};
-  }, [id]);
+  }, [formid]);
 
   async function handleUpdate(values) {
     try {
       setSubmitting(true); // submitting status set to true
-      await apiRequest(`/form?id=eq.${id}`, "PATCH", {
+      await apiRequest(`/form?id=eq.${formid}`, "PATCH", {
         name: values.name,
         description: values.description,
       });
